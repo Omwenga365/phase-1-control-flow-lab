3acc896fe7425156f3d5ce92aaf507739b852f3a
Esbon Omwenga <victoromwenga365@gmail.com>
2022-11-25 10:17:00 -0500

    done Assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+if(feet<=400){
+  return 'If this one is on me!'
+  scuberGreetingForFeet(199);
+} else if(feet>=2001 && feet <=2500) {
+  return 'I will gladly take yourthirty bucks.'
+  scuberGreetingForFeet(2001);
+}else if (feet > 2500) {
+  return 'No can do.'
+  scuberGreetingForFeet(2501);
+}
+function ternaryCheckCity (city) {
+  if (city == "NYC") {
+    return "ok,sounds good."
+  }else if (city != "NYC") {
+    return 'no go'
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip)
+  switch (tip) {
+    case "generous":return 'Thank you so much.';
+    case "not as generous":return 'Thank you';
+    default:return 'Bye';
+  }
 }
